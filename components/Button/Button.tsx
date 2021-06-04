7e355d89c055e0f9790b18ae1d0e0dd8bbce6afb
redgreengrey <redgreengrey@yandex.ru>
2021-06-04 15:53:35 +0400

    add: component Button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
+import cn from "classnames";
+import ArrowIcon from "./arrow.svg";
+
+export const Button = ({
+    appearence,
+    arrow = "none",
+    children,
+    className,
+    ...props
+}: ButtonProps): JSX.Element => {
+    return (
+        <button
+            className={cn(styles.button, className, {
+                [styles.primary]: appearence === "primary",
+                [styles.ghost]: appearence === "ghost",
+            })}
+            {...props}
+        >
+            {children}
+            {arrow !== "none" && (
+                <span
+                    className={cn(styles.arrow, {
+                        [styles.down]: arrow === "down",
+                    })}
+                >
+                    <ArrowIcon />
+                </span>
+            )}
+        </button>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { Button, Htag } from "../components";
+
 export default function Home(): JSX.Element {
-    return <div>testestest</div>;
+    return (
+        <>
+            <Htag tag="h1">текст</Htag>
+            <Button appearence="primary" arrow="right">
+                кнопка
+            </Button>
+            <Button appearence="ghost" arrow="right">
+                кнопка
+            </Button>
+        </>
+    );
 }
