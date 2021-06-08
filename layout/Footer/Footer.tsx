5cf12c73cf65061877e12384da69c13b57c531dc
redgreengrey <redgreengrey@yandex.ru>
2021-06-08 23:50:15 +0400

    add: added Footer component, date-fns package

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-    return <div {...props}>footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+    return (
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+            <a href="#" target="blank">
+                Пользовательское соглашение
+            </a>
+            <a href="#" target="blank">
+                Политика конфиденциальности
+            </a>
+        </footer>
+    );
 };
