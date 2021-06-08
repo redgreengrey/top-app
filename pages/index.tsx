47bca9fa1e6d43cc382b8d814624879db052b68c
redgreengrey <redgreengrey@yandex.ru>
2021-06-08 13:58:09 +0400

    add: Layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { Button, Htag, P, Rating } from "../components";
+import { Layout } from "../layout/Layout";
 
 export default function Home(): JSX.Element {
     const [rating, setRating] = useState<number>(3);
 
     return (
-        <>
+        <Layout>
             <Htag tag="h1">{rating}</Htag>
             <Button appearence="primary" arrow="right">
                 button
@@ -20,6 +21,6 @@ export default function Home(): JSX.Element {
             <P>Средний</P>
             <P size="s">Маленький</P>
             <Rating isEditable rating={rating} setRating={setRating} />
-        </>
+        </Layout>
     );
 }
