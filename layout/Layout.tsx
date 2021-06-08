07efcb62dc9c82b553001006638dcc30dbd05354
redgreengrey <redgreengrey@yandex.ru>
2021-06-08 14:18:02 +0400

    add: withLayout HOC

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -17,3 +17,13 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
         </div>
     );
 };
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <Layout>
+                <Component {...props} />
+            </Layout>
+        );
+    };
+};
