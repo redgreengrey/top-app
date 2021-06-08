40a50a4a4b20f109c7315bd0192404db9811e68b
redgreengrey <redgreengrey@yandex.ru>
2021-06-08 23:09:06 +0400

    add: added layout for Layout component

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -8,13 +8,11 @@ import { FunctionComponent } from "react";
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <div>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>{children}</div>
-            </div>
-            <Footer />
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.body}>{children}</div>
+            <Footer className={styles.footer} />
         </div>
     );
 };
