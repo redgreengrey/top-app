d5590541417dbcc45a92f63514cd9c5fb0d39583
redgreengrey <redgreengrey@yandex.ru>
2021-06-16 16:08:20 +0400

    add: products page additions

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,14 +1,14 @@
-import { HhData, Htag, Tag } from "../../components";
+import { Advantages, HhData, Htag, P, Tag } from "../../components";
 import { TopPageComponentProps } from "./TopPageComponent.props";
 import styles from "./TopPageComponent.module.css";
 import { TopLevelCategory } from "../../interfaces/page.interface";
+import React from "react";
 
 export const TopPageComponent = ({
     page,
     products,
     firstСategory,
 }: TopPageComponentProps): JSX.Element => {
-    console.log(firstСategory);
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>
@@ -27,7 +27,20 @@ export const TopPageComponent = ({
                     hh.ru
                 </Tag>
             </div>
-            {firstСategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
+            {firstСategory === TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+            {page.advantages && page.advantages.length > 0 && (
+                <>
+                    <Htag tag="h2">Преимущества</Htag>
+                    <Advantages advantages={page.advantages}></Advantages>
+                </>
+            )}
+            {page.seoText && <P>{page.seoText}</P>}
+            <Htag tag="h2">Получаемые навыки</Htag>
+            {page.tags.map((t) => (
+                <Tag key={t} color="primary">
+                    {t}
+                </Tag>
+            ))}
         </div>
     );
 };
