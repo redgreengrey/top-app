5c8c8a6e506b13151d3b30f894ea89b0b34aab18
redgreengrey <redgreengrey@yandex.ru>
2021-06-17 15:45:26 +0400

    fix: delete unnecessary curly brace

--- components/Search/Search.props.ts
+++ components/Search/Search.props.ts
@@ -2,4 +2,3 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
 export interface SearchProps
     extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
-}
