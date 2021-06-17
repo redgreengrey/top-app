c405170703b409702fe5cc19e902ca36a92bebe2
redgreengrey <redgreengrey@yandex.ru>
2021-06-17 15:58:43 +0400

    add: Product component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,4 +1,4 @@
-import { Advantages, HhData, Htag, Sort, Tag } from "../../components";
+import { Advantages, HhData, Htag, Product, Sort, Tag } from "../../components";
 import { TopPageComponentProps } from "./TopPageComponent.props";
 import styles from "./TopPageComponent.module.css";
 import { TopLevelCategory } from "../../interfaces/page.interface";
@@ -32,7 +32,7 @@ export const TopPageComponent = ({
                 <Sort sort={sort} setSort={setSort} />
             </div>
             <div>
-                {sortedProducts && sortedProducts.map((p) => <div key={p._id}>{p.title}</div>)}
+                {sortedProducts && sortedProducts.map((p) => <Product key={p._id} product={p} />)}
             </div>
             <div className={styles.hhTitle}>
                 <Htag tag="h2">Вакансии - {page.category}</Htag>
