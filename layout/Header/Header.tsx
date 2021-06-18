0b68b316bb57792d42c687ea9245a2bd0fb5f6a5
redgreengrey <redgreengrey@yandex.ru>
2021-06-18 18:12:14 +0400

    fix: stylelint fix

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -3,5 +3,9 @@ import styles from "./Header.module.css";
 import cn from "classnames";
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
-    return <div {...props}>header</div>;
+    return (
+        <div className={styles.header} {...props}>
+            header
+        </div>
+    );
 };
