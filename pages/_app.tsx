3c575a1f33449757753590362c6fc935a3aa2f0e
redgreengrey <redgreengrey@yandex.ru>
2021-06-28 12:52:24 +0400

    add: yandex metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
 import "../styles/globals.css";
+import ym from "react-yandex-metrika";
+import { YMInitializer } from "react-yandex-metrika";
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+    router.events.on("routeChangeComplete", (url: string) => {
+        if (typeof window !== "undefined") {
+            ym("hit", url);
+        }
+    });
+
     return (
         <>
             <Head>
                 <title>OwlTop</title>
                 <link rel="icon" href="/favico.ico" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://mc.yandex.ru" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
                     rel="stylesheet"
@@ -16,6 +25,7 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
                 <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
                 <meta property="og:locale" content="ru_RU" />
             </Head>
+            <YMInitializer accounts={[]} options={{ webvisor: true, defer: true }} version="2" />
             <Component {...pageProps} />
         </>
     );
